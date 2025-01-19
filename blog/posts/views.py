bdc7ce788be0b309f160f698a07980d0207284a9
dzuli98 <filipovicjulija98@gmail.com>
2025-01-19 23:24:04 +0100

    add django_blog project

---
+++ blog/posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {'posts': Post.objects.all()})
+
+def post(request, pk):
+    posts = Post.objects.get(id=pk)
+    return render(request, 'post.html', {'posts': posts})
